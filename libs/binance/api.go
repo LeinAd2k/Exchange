582589aa8cf8512e6a2364b4aa01feb37fd18ea1
yang <kang.yang@bitocean.org>
2020-02-10 19:51:57 +0800

    add binance v3 api

--- libs/binance/api.go
+++ libs/binance/api.go
@@ -1,1 +1,39 @@
 package binance
+
+import (
+	"encoding/json"
+
+	"gopkg.in/resty.v1"
+)
+
+const baseURL = "https://api.binance.com/api/v3"
+
+// Ping ...
+func Ping() (map[string]interface{}, error) {
+	resp, err := resty.R().Get(baseURL + "/ping")
+	if err != nil {
+		return nil, err
+	}
+	var m map[string]interface{}
+	err = json.Unmarshal([]byte(resp.Body()), &m)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+// ServerTimestamp ...
+func ServerTimestamp() (map[string]interface{}, error) {
+	resp, err := resty.R().Get(baseURL + "/time")
+	if err != nil {
+		return nil, err
+	}
+	var m map[string]interface{}
+	err = json.Unmarshal([]byte(resp.Body()), &m)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
