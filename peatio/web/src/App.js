2b3ee6bf639c8cfa61cc70e91b84c0cd068bb87b
FlowerWrong <sysuyangkang@gmail.com>
2020-03-30 22:08:42 +0800

    add order book ui demo

--- peatio/web/src/App.js
+++ peatio/web/src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { Button } from "antd";
 
 import logo from "./logo.svg";
 import "./App.css";
 
-import DepthChart from "./components/Depth.jsx";
+// import DepthChart from "./components/Depth.jsx";
+import OrderBookUI from "./components/OrderBookUI.jsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button type="primary">Button</Button>
-        <DepthChart title={"Order Book Depth"} />
+        <OrderBookUI title={"Order Book Depth"} />
       </header>
     </div>
   );
---
+++ peatio/web/src/components/OrderBookUI.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useCallback, useEffect } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
+import { Decimal } from "decimal.js";
+import Market from "../book/Market";
+
+import OrderBook from "../tradingUI/OrderBook";
+
+const market = new Market();
+
+function OrderBookUI(props) {
+  const [socketUrl] = useState("ws://127.0.0.1:6389");
+  const [sendMessage, lastMessage, readyState] = useWebSocket(socketUrl);
+  const [book, setBook] = useState({
+    asks: [],
+    bids: []
+  });
+
+  const subData = {
+    cmd: "sub",
+    payload: {
+      name: "bitmex_XBTUSD"
+    }
+  };
+  const handleClickSendMessage = useCallback(
+    () => sendMessage(JSON.stringify(subData)),
+    [sendMessage, subData]
+  );
+
+  const instrument = "bitmex_XBTUSD";
+  let updateCounter = 0;
+  market.open(instrument);
+  market.onupdate = function(event) {
+    if (event.initFlag) {
+      console.log("Init render bids", event.bids.length);
+      console.log("Init render asks", event.asks.length);
+    }
+    if (event.initFlag || updateCounter > 5) {
+      const bids = event.bids;
+      const asks = event.asks;
+
+      setBook({
+        asks: asks,
+        bids: bids
+      });
+      updateCounter = 0;
+    } else {
+      updateCounter += 1;
+    }
+  };
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      const data = JSON.parse(lastMessage.data);
+
+      if (data.cmd === "partial") {
+        market.initOrderBook(
+          instrument,
+          data.asks.map(function(price_vol) {
+            return [
+              new Decimal(price_vol[0]).toNumber(),
+              new Decimal(price_vol[1]).toNumber()
+            ];
+          }),
+          data.bids.map(function(price_vol) {
+            return [
+              new Decimal(price_vol[0]).toNumber(),
+              new Decimal(price_vol[1]).toNumber()
+            ];
+          })
+        );
+      } else if (data.cmd === "update") {
+        data.asks.forEach(function(price_vol) {
+          market.update(
+            instrument,
+            "S",
+            new Decimal(price_vol[0]).toNumber(),
+            new Decimal(price_vol[1]).toNumber()
+          );
+        });
+
+        data.bids.forEach(function(price_vol) {
+          market.update(
+            instrument,
+            "B",
+            new Decimal(price_vol[0]).toNumber(),
+            new Decimal(price_vol[1]).toNumber()
+          );
+        });
+      }
+    }
+  }, [lastMessage]);
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed"
+  }[readyState];
+
+  return (
+    <div>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      <button
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Click Me to subscribe data
+      </button>
+      <OrderBook
+        asks={book.asks}
+        bids={book.bids}
+        getPrice={entry => entry[0]}
+        getSize={entry => entry[1]}
+      />
+      {/* <OrderBook asks={book.asks} bids={book.bids} /> */}
+    </div>
+  );
+}
+
+export default OrderBookUI;
